064c0688bbac575b24266f2ad7475e7222874507
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-08-24 14:42:18 +0200

    Add components to App
    - SitesComponent, DashboardComponent for the app
    - MaterialsModule, BrowserAnimationsModule for Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SitesComponent } from './sites/sites.component';
+import { MaterialModule } from './material.module'; // Angular Material Module
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SitesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
