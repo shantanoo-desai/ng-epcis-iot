52ec4c6c7cf5bd6420875d52d18b1034bceb3381
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-08-24 14:39:13 +0200

    Add Routes to the App
    - Default landing route to Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SitesComponent } from './sites/sites.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'sites',
+    component: SitesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
