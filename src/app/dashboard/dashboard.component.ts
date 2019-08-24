6d68590cb5a506531a96652250c7585b4957dccb
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-08-24 14:46:53 +0200

    Add Initial Layout for Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  subscribedSites = [{
+    company: 'acme',
+    siteName: 'factory',
+    countryCode: 'DE',
+    city: 'frankfurt',
+    sensors: [
+      {
+        mac: '00:11:DE:AD:BE:EF',
+        bizLocation: 'location1'
+      },
+      {
+        mac: '11:11:DE:AD:BE:EF',
+        bizLocation: 'location2'
+      }
+    ]
+  },
+  {
+    company: 'acme',
+    siteName: 'factory',
+    countryCode: 'DE',
+    city: 'frankfurt',
+    sensors: [
+      {
+        mac: '00:11:DE:AD:BE:EF',
+        bizLocation: 'location1'
+      },
+      {
+        mac: '11:11:DE:AD:BE:EF',
+        bizLocation: 'location2'
+      }
+    ]
+  }];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  removeSite(index: number) {
+    this.subscribedSites.splice(index, 1);
   }
 
 }
